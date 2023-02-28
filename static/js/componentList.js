3edcb254aa13b85168eaf96ad07e418f2e549a71
STechQ <stech.quick@yandex.com>
2023-02-28 12:46:03 +0000

    deploy for test

--- static/js/componentList.js
+++ static/js/componentList.js
@@ -672,7 +672,7 @@ var componentOptions = {
             type: Boolean
         },
         elevation: {
-            string: "number | string",
+            type: "number | string",
             options: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24"],
             editorDefaultValue: "0"
         },
@@ -4837,7 +4837,7 @@ nextIcon = 'mdi-plus'}`
             type: String
         },
         tagType: {
-            tpye: String,
+            type: String,
             editorDefaultValue: "'span'",
             options: ["'span'", "'paragraph'"]
         },
