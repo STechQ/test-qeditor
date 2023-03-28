7576ff5e6b60bc6a21e6eeeb8ba241b96e940505
STechQ <stech.quick@yandex.com>
2023-03-28 12:39:07 +0000

    deploy for test

--- packages/websdk/src/implementation/StyleManager.d.ts
+++ packages/websdk/src/implementation/StyleManager.d.ts
@@ -3,16 +3,19 @@ import { IStyleManager, StyleCB } from "../../../../common/shrimp/interfaces/qui
 export declare class StyleManager implements IStyleManager {
     private static styleTag;
     static createStyleTag(): void;
+    private static index;
+    private static cssIndexDict;
     static compStyleDict: Record<string, StyleCB>;
     static matchComponentStyles: (compCollection: IComponentCollection) => void;
-    static matchStyleValue: ({ selector, selectorValue, styleValue, breakPoint, }: {
+    static matchStyleValue: ({ selector, selectorValue, styleValue, breakPoint, compType }: {
         selector: string;
         selectorValue: string;
         styleValue: string;
         breakPoint?: string | undefined;
+        compType: string;
     }) => void;
-    static addStyleRule: (selector: string, value: string, breakPoint?: string | undefined) => void;
-    static deleteStyleRule: (selector: string) => number;
+    static addStyleRule: (selector: string, selectorValue: string, compType: string, breakPoint?: string | undefined) => void;
+    static deleteStyleRule: (selector: string, selectorValue: string, compType: string) => number;
     static fromCamelCase: (message: string) => string;
     static matchSizeMinValue(sizeString?: string): string;
     static matchSizeMaxValue(sizeString?: string): string;
