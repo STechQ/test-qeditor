394a35b47fd2d3fa7a93eb3d5407abb01766610a
STechQ <stech.quick@yandex.com>
2023-04-12 13:19:53 +0000

    deploy for test

--- packages/websdk/src/components/providers/ChartComponents.d.ts
+++ packages/websdk/src/components/providers/ChartComponents.d.ts
@@ -1,7 +1,19 @@
 declare const chartLibrary: {
-    Doughnut: any;
-    Bar: any;
-    Line: any;
+    Bar: {
+        importFunc: () => Promise<void>;
+        loaded: boolean;
+        module: undefined;
+    };
+    Doughnut: {
+        importFunc: () => Promise<void>;
+        loaded: boolean;
+        module: undefined;
+    };
+    Line: {
+        importFunc: () => Promise<void>;
+        loaded: boolean;
+        module: undefined;
+    };
 };
 export { chartLibrary };
 //# sourceMappingURL=ChartComponents.d.ts.map
