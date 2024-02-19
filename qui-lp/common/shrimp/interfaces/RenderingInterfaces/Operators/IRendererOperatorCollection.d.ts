b35a8ec5e8424c8f6775b7ae78363b1b151f6da3
STechQ <stech.quick@yandex.com>
2024-02-19 14:00:38 +0000

    deploy for test

--- qui-lp/common/shrimp/interfaces/RenderingInterfaces/Operators/IRendererOperatorCollection.d.ts
+++ qui-lp/common/shrimp/interfaces/RenderingInterfaces/Operators/IRendererOperatorCollection.d.ts
@@ -2,10 +2,12 @@ import { IComponentOperator } from "./IComponentOperator";
 import { IEventOperator } from "./IEventOperator";
 import { ILoadingOperator } from "./ILoadingOperator";
 import { IAuthenticationOperator } from "./IAuthenticationOperator";
+import { IPermanentStoreOperator } from "./IPermanentStoreOperator";
 export interface IRendererOperatorCollection {
     eventOperator?: IEventOperator;
     componentOperator?: IComponentOperator;
     loadingOperator: ILoadingOperator;
     authenticationOperator?: IAuthenticationOperator;
+    permanentStoreOperator?: IPermanentStoreOperator;
 }
 //# sourceMappingURL=IRendererOperatorCollection.d.ts.map
